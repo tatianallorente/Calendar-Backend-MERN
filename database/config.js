0b6120fa0e7476a58c61429dd619664b175474b9
Tatiana Llorente <tatianallorente@gmail.com>
2021-01-27 12:29:46 +0100

    Minor fix

--- database/config.js
+++ database/config.js
@@ -19,9 +19,3 @@ const dbConnection = async() => {
 module.exports = {
     dbConnection
 }
-
-/*
-mern_user
-0JQrZYN48BVi69A7
-mongodb+srv://mern_user:0JQrZYN48BVi69A7@cluster0.www53.mongodb.net
-*/
